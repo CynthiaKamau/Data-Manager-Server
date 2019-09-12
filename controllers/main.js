299d09de5546ef097cdda7f88b24a7bd842ad0c8
CynthiaKamau <cynthia.kamau@students.jkuat.ac.ke>
2019-09-12 17:04:54 +0300

    second commit

--- controllers/main.js
+++ controllers/main.js
@@ -1,5 +1,5 @@
 const  getTableData = (req, res, db) => {
-    db.select('*').from('testtable1')
+    db.select('*').from('testtable')
         .then(items => {
             if (items.length){
                 res.json(items)
@@ -7,41 +7,41 @@ const  getTableData = (req, res, db) => {
                 res.json({dataExsists: 'false'})
             }
         })
-        .catch(err => res.status(400).json({dbError: 'db error'}))
-}
+        .catch(err => console.log(err))
+};
 const postTableData = (req, res, db) => {
-    const {freezer, chamber, rack, tray,box, box_label, sample_type, description, project, responsible_personel, box_barcode} = req.body
-    const added = new Date()
-    db('testtable1').insert({freezer, chamber, rack, tray, box,box_label, sample_type, description, project, responsible_personel, box_barcode, added})
+    const {freezer, chamber, rack, tray,box, box_label, sample_type, description, project, responsible_personel, box_barcode} = req.body;
+    const added = new Date();
+    db('testtable').insert({freezer, chamber, rack, tray, box,box_label, sample_type, description, project, responsible_personel, box_barcode, added})
         .returning('x')
         .then(item => {
             res.json(item)
         })
-        .catch(err => res.status(400).json({dbError: 'db error'}))
-}
+        .catch(err => console.log(err))
+};
 
 const putTableData = (req, res, db) => {
-    const {id, freezer, chamber, rack,tray, box, box_label,sample_type, description, project, responsible_personel, box_barcode} = req.body
-    db('testtable1').where({id}).update({freezer, chamber, rack,tray, box_label,sample_type, description, project, responsible_personel, box_barcode})
+    const {id, freezer, chamber, rack,tray, box, box_label,sample_type, description, project, responsible_personel, box_barcode} = req.body;
+    db('testtable').where({id}).update({freezer, chamber, rack,tray, box_label,sample_type, description, project, responsible_personel, box_barcode})
         .returning('*')
         .then(item => {
             res.json(item)
         })
                 .catch(err =>res.status(400).json({dbError: 'db error'}))
 
-}
+};
 const deleteTableData = (req, res,db) => {
-    const {id} = req.body
-    db('testtable1').where({id}).del()
+    const {id} = req.body;
+    db('testtable').where({id}).del()
         .then(() => {
             res.json({delete: 'true'})
         })
         .catch(err => res.status(400).json({dbError: 'db error'}))
-}
+};
 
 module.exports = {
     getTableData,
     postTableData,
     putTableData,
     deleteTableData
-}
+};
--- server.js
+++ server.js
@@ -1,33 +1,33 @@
-const  express = require('express')
+const  express = require('express');
 
 //use process .env variables to keep private variables,
-require('dotenv').config()
+require('dotenv').config();
 
 //Express Middleware
-const helmet = require('helmet') //create headers to protect security attacks
-const bodyParser = require('body-parser') //turns response into usable format
-const cors = require('cors') // allows/disables cross-site communication
-const morgan = require('morgan') //logs requests
+const helmet = require('helmet'); //create headers to protect security attacks
+const bodyParser = require('body-parser'); //turns response into usable format
+const cors = require('cors'); // allows/disables cross-site communication
+const morgan = require('morgan'); //logs requests
 
 //connection to localhost
 var db = require('knex')({
-   client : 'pg',
+   client : 'pg', //connection to postgress
    connection:{
     host : '127.0.0.1',
-        user : '',
-        password : '',
-        database : 'bioinformaticsdata'
+        user : 'shiro',
+        password : 'zoom',
+        database : 'crudpractise1'
     }
 });
 
 //controllers -aka, the db queries
-const main = require('./controllers/main')
+const main = require('./controllers/main');
 
 //App
-const app = express()
+const app = express();
 
 //App Middleware
-const whitelist = ['https://localhost:3001']
+const whitelist = ['https://localhost:3001'];
 const corsOptions = {
     origin: function (origin, callback) {
         if (whitelist.indexOf(origin) !== -1 || !origin) {
@@ -37,21 +37,21 @@ const corsOptions = {
         }
 
     }
-}
-app.use(helmet())
-app.use(cors(corsOptions))
-app.use(bodyParser.json())
-app.use(morgan('combined')) //use 'tiny' or 'combined'
+};
+app.use(helmet());
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(morgan('combined')); //use 'tiny' or 'combined'
 
 //App Routes -Auth
-app.get('/', (req, res) => res.send('hello world'))
-app.get('/crud', (req, res) => main.getTableData(req, res, db))
-app.post('/crud', (req, res) => main.postTableData(req, res,db))
-app.put('/crud', (req, res) =>main.putTableData(req, res,db))
-app.delete('/crud', (req, res) => main.deleteTableData(req, res,db))
+app.get('/', (req, res) => res.send('hello world'));
+app.get('/crud', (req, res) => main.getTableData(req, res, db));
+app.post('/crud', (req, res) => main.postTableData(req, res,db));
+app.put('/crud', (req, res) =>main.putTableData(req, res,db));
+app.delete('/crud', (req, res) => main.deleteTableData(req, res,db));
 
 // App Server Connection
 app.listen(process.env.PORT || 3000, () => {
     console.log('APP IS RUNNING ON PORT ${process.env.PORT || 3000}')
-})
+});
 
